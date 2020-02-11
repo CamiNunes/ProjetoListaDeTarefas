866bc79873bfbeb0cfa3fd574b9095600d68bdde
CamiNunes <cvn.camila@gmail.com>
2020-02-11 20:57:52 -0300

    Percorrendo Array com forEach;

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -5,8 +5,19 @@ let data = [{
     id: 2,
     title: "Estudar CSS"
 },{
-    id: 2,
+    id: 3,
     title: "Estudar Javascript"
+},{
+    id: 4,
+    title: "Estudar Azure DevOps"
 }];
 
-console.log(data);
+data.forEach(task =>{
+    let li = document.createElement('li');
+
+    li.innerHTML = `
+        <input type="checkbox" id="task-${task.id}"><label for="${task.id}">${task.title}</label>
+    `;
+
+    document.querySelector('.todo').append(li);
+});
