c1d4c409c87c71dbf5ef1846bcc7c3089bf0b15a
CamiNunes <cvn.camila@gmail.com>
2020-02-11 21:55:22 -0300

    Adicionando Elementos em um Array com Push;

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,37 +1,39 @@
-let data = [{
-    id: 1,
-    title: "Estudar HTML"
-},{
-    id: 2,
-    title: "Estudar CSS"
-},{
-    id: 3,
-    title: "Estudar Javascript"
-},{
-    id: 4,
-    title: "Estudar Azure DevOps"
-}];
+let data = [];
 
-data.forEach(task =>{
-    let li = document.createElement('li');
+function renderTodo() {
 
-    li.innerHTML = `
-        <input type="checkbox" id="task-${task.id}"><label for="${task.id}">${task.title}</label>
-    `;
+    document.querySelector('.todo').innerHTML = '';
 
-    li.querySelector('input').addEventListener("change", e => {
-        if (e.target.checked){
-            li.classList.add('complete');
-        } else {
-            li.classList.remove('complete');
-        }
+    data.forEach(task =>{
+        let li = document.createElement('li');
+    
+        li.innerHTML = `
+            <input type="checkbox" id="task-${task.id}"><label for="${task.id}">${task.title}</label>
+        `;
+    
+        li.querySelector('input').addEventListener("change", e => {
+            if (e.target.checked){
+                li.classList.add('complete');
+            } else {
+                li.classList.remove('complete');
+            }
+        });
+    
+        document.querySelector('.todo').append(li);
     });
+}
 
-    document.querySelector('.todo').append(li);
-});
 
 document.querySelector('#new-task').addEventListener('keyup', e => {
     if(e.key === 'Enter'){
-        console.log(e.target.value);
+        data.push({
+            id: data.length+1,
+            title: e.target.value
+
+        });
+        e.target.value = "";
+        renderTodo();
     }
 });
+
+renderTodo();
